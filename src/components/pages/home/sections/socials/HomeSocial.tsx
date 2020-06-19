addf83f406053290ecbc55abc9d9bc26403006d9
DayColor <2005alonlivne@gmail.com>
2020-06-19 18:47:21 +0300

    feat(HomeSocial): socials routes

--- src/components/pages/home/sections/socials/HomeSocial.tsx
+++ src/components/pages/home/sections/socials/HomeSocial.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export interface HomeSocialProps {
+  route: string;
   title: string;
   desc: string;
   src: string;
 }
 
 const HomeSocial: React.FC<HomeSocialProps> = (props: HomeSocialProps) => {
-  const { title, desc, src } = props;
+  const { route, title, desc, src } = props;
 
   return (
     <div className="z-10 lg:w-1/3 px-10 py-6">
-      <div className="bg-white shadow-soft-xl rounded-lg p-5 flex justify-center flex-col items-center">
-        <img className="rounded" width={140} src={src} alt="title" />
-        <h3 className="mt-3 text-xl font-semibold">{title}</h3>
-        <p className="mt-3">{desc}</p>
-      </div>
+      <Link to={route}>
+        <div className="transition-transform duration-200 ease-in-out transform hover:-translate-y-5 bg-white shadow-soft-xl rounded-lg p-5 flex justify-center flex-col items-center">
+          <img className="rounded" width={140} src={src} alt="title" />
+          <h3 className="mt-3 text-xl font-semibold">{title}</h3>
+          <p className="mt-3">{desc}</p>
+        </div>
+      </Link>
     </div>
   );
 };
--- src/components/pages/home/sections/socials/HomeSocials.tsx
+++ src/components/pages/home/sections/socials/HomeSocials.tsx
@@ -11,31 +11,37 @@ import verified from '../../../../../assets/home/socials/verified.png';
 
 const homeSocials: HomeSocialProps[] = [
   {
+    route: '/products/verified',
     title: 'Verified',
     desc: 'Lorem ipsum dolor sit amet consectetur adipiscing elit.',
     src: verified,
   },
   {
+    route: '/products/forbes',
     title: 'Forbes',
     desc: 'Lorem ipsum dolor sit amet consectetur adipiscing elit.',
     src: forbes,
   },
   {
+    route: '/products/tiktok',
     title: 'TikTok',
     desc: 'Lorem ipsum dolor sit amet consectetur adipiscing elit.',
     src: tiktok,
   },
   {
+    route: '/products/followers',
     title: '+5,000 Followers',
     desc: 'Lorem ipsum dolor sit amet consectetur adipiscing elit.',
     src: followers5k,
   },
   {
+    route: '/products/followers',
     title: '+10,000 Followers',
     desc: 'Lorem ipsum dolor sit amet consectetur adipiscing elit.',
     src: followers10k,
   },
   {
+    route: '/products/followers',
     title: '+25,000 Followers',
     desc: 'Lorem ipsum dolor sit amet consectetur adipiscing elit.',
     src: followers25k,
@@ -51,12 +57,12 @@ const HomeSocials: React.FC = () => {
           <p>Do not wait for tomorrow - start a new life today</p>
         </div>
         <div className="home--socials relative flex justify-between items-center flex-wrap">
-          <HomeSocialsDots className="top-0 right-0 translate-x-16 -translate-y-16" />
+          <HomeSocialsDots className="top-0 right-0 translate-x-24 -translate-y-24" />
           {homeSocials.map(social => (
             <HomeSocial key={social.title} {...social} />
           ))}
           <HomeSocialSlider />
-          <HomeSocialsDots className="bottom-0 left-0 -translate-x-16 translate-y-16" />
+          <HomeSocialsDots className="bottom-0 left-0 -translate-x-24 translate-y-24" />
         </div>
       </div>
     </section>
