ba5558a5a46c580f8a442a8241b910ee2e969246
DayColor <2005alonlivne@gmail.com>
2020-06-25 00:28:12 +0300

    chore(Product): added price

--- src/components/pages/product/Product.tsx
+++ src/components/pages/product/Product.tsx
@@ -4,29 +4,33 @@ export interface ProductProps {
   title: string;
   desc: string;
   src: string;
+  price: number;
   readyToOrder: string;
 }
 
 const Product: React.FC<ProductProps> = (props: ProductProps) => {
-  const { title, desc, src, readyToOrder } = props;
+  const { title, desc, src, price, readyToOrder } = props;
 
   return (
     <div className="container text-center">
       <div className="lg:w-3/5 mx-auto">
-        <div className="mb-32">
+        <div className="mb-16">
           <h1 className="text-3xl font-bold mb-3">{title}</h1>
           <p>{desc}</p>
         </div>
-        <div className="mb-32">
-          <img width={350} className="mx-auto" src={src} alt={title} />
-        </div>
         <div className="mb-16">
-          <h1 className="text-3xl font-semibold mb-3">Ready to order?</h1>
-          <p>{readyToOrder}</p>
+          <img width={350} className="mx-auto" src={src} alt={title} />
         </div>
         <button type="button" className="btn px-16">
           Buy Now!
         </button>
+        <span className="text-sm block mt-2 opacity-75">
+          USD {price.toLocaleString()}
+        </span>
+        <div className="mt-16">
+          <h1 className="text-3xl font-semibold mb-3">Ready to order?</h1>
+          <p>{readyToOrder}</p>
+        </div>
       </div>
     </div>
   );
