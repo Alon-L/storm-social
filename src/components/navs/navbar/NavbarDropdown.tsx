d90cf3093f54ff650d38f2147190a0a953e6d575
DayColor <2005alonlivne@gmail.com>
2020-06-27 15:44:00 +0300

    chore(NavbarDropdown): fixed trigger

--- src/components/navs/navbar/NavbarDropdown.tsx
+++ src/components/navs/navbar/NavbarDropdown.tsx
@@ -1,6 +1,7 @@
 import { faChevronDown, faChevronUp } from '@fortawesome/pro-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { MouseEvent, PropsWithChildren, useState } from 'react';
+import React, { PropsWithChildren, useState } from 'react';
+import OutsideClickHandler from 'react-outside-click-handler';
 
 export interface NavbarDropdownProps {
   name: string;
@@ -13,33 +14,32 @@ const NavbarDropdown: React.FC<NavbarDropdownProps> = (
 
   const [active, setActive] = useState(false);
 
-  document.body.addEventListener('click', () => setActive(false));
-
   return (
-    <div className="relative">
-      <button
-        type="button"
-        onClick={(e: MouseEvent<HTMLButtonElement>) => {
-          e.stopPropagation();
-          setActive(!active);
-        }}
-        className="text-sm opacity-70 hover:opacity-100 transition-opacity duration-200 ease-in-out font-semibold uppercase"
-      >
-        {name}{' '}
-        <FontAwesomeIcon
-          className="ml-2"
-          icon={active ? faChevronUp : faChevronDown}
-        />
-      </button>
-      <div
-        className={`shadow-md
+    <OutsideClickHandler onOutsideClick={() => setActive(false)}>
+      <div className="relative">
+        <button
+          type="button"
+          onClick={() => {
+            setActive(!active);
+          }}
+          className="text-sm opacity-70 hover:opacity-100 transition-opacity duration-200 ease-in-out font-semibold uppercase"
+        >
+          {name}{' '}
+          <FontAwesomeIcon
+            className="ml-2"
+            icon={active ? faChevronUp : faChevronDown}
+          />
+        </button>
+        <div
+          className={`shadow-md
          rounded-lg absolute bg-white transform translate-y-2 z-50 ${
            !active && 'hidden'
          }`}
-      >
-        {children}
+        >
+          {children}
+        </div>
       </div>
-    </div>
+    </OutsideClickHandler>
   );
 };
 
